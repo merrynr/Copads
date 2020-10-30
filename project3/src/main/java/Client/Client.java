4fb7795b806db2bc2702fc7d5be5f09d5a0889e5
William Anderson <andersonwilliam236@gmail.com>
2020-10-30 19:57:46 -0400

    An attempt at client connections

--- project3/src/main/java/Client/Client.java
+++ project3/src/main/java/Client/Client.java
@@ -7,10 +7,14 @@ import java.util.Set;
 public class Client {
 
     public static Queue<String> msgQueue;
+    public static Unicast unicast;
+    public static String name = System.getenv("HOSTNAME");
 
     public static void main(String args[]) {
 
         System.out.println("Client program started...");
+        unicast = new Unicast();
+        unicast.start();
 
         Scanner reader = new Scanner(System.in);
         String cmd;
@@ -31,17 +35,33 @@ public class Client {
         }
     }
 
-    private String pickRandom() {
-        return null;
+    public static void addMessage(String message) {
+        synchronized (msgQueue) {
+            msgQueue.add(message);
+        }
     }
 
     //MOVE TO THREADED CONNECTION
-    private static void sendMessage(String message) {
+    private static void processMessage(String message) {
+
+        String[] splitMessage = message.split("/");
+        switch (splitMessage[1]) {
+            case "LOG_QUERY":
+
+                break;
+
+            case "LOG_REQUEST":
+
+                break;
+        }
+
 
         //SEND TO FIRST NODE IN NODELIST
             //IF GOOD RESPONSE, BREAK
             //IF RECEIVE NO LEADER, PRINT FAILURE
             //IF NO RESPONSE, LOOP
     }
+
+
 
 }
---
+++ project3/src/main/java/Client/Connections.java
@@ -0,0 +1,91 @@
+package Client;
+
+
+import Server.Node;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.EOFException;
+import java.io.IOException;
+import java.net.*;
+
+/** Classes for managing incoming connections from other peers (mainly) */
+
+class Unicast extends Thread {
+
+    private static int port;
+
+    ServerSocket listenerSocket;
+
+    public Unicast() {
+        port = 9092;
+    }
+
+    /** Server.Unicast server */
+    public void run() {
+        try {
+            listenerSocket = new ServerSocket(port);
+            System.out.println("connection server started ...\n"); //~Print~//
+            while (true) {
+                Socket peerSocket = listenerSocket.accept();
+                UnicastConnection c = new UnicastConnection(peerSocket);
+                c.start();
+            }
+        } catch (IOException e) {
+            System.out.println("Listen :" + e.getMessage());
+        }
+    }
+
+    public static void sendMsg(String message, String peerName) throws IOException{
+        Socket senderSocket = new Socket(peerName, port);
+        DataOutputStream out = new DataOutputStream(senderSocket.getOutputStream());
+
+        out.writeUTF(message);
+        out.flush();
+
+        out.close();
+
+        if (senderSocket != null) {
+            try {
+                senderSocket.close();
+            } catch (IOException e) {
+                System.out.println("close:" + e.getMessage());
+            }
+        }
+
+    }
+}
+
+
+class UnicastConnection extends Thread {
+
+    private DataInputStream in;
+    private Socket peerSocket;
+
+    public UnicastConnection(Socket aPeerSocket) {
+        try {
+            peerSocket = aPeerSocket;
+            in = new DataInputStream(peerSocket.getInputStream());
+        } catch (IOException e) {
+            System.out.println("Connection:" + e.getMessage());
+        }
+    }
+
+    /** Server.Unicast listener */
+    public void run() {
+        try {
+            String message = in.readUTF();
+
+            Client.addMessage(message);
+
+        } catch (EOFException e) {
+            System.out.println("EOF:" + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("IO:" + e.getMessage());
+        } finally {
+            try {
+                peerSocket.close();
+            } catch (IOException e) {/*close failed*/}
+        }
+    }
+}
